5fc42d6148ab2b15f545780185735fef61c15521
Rafael Kerr <rafael.kerr@gmail.com>
2014-09-15 16:36:29 -0300

    add all and commit

---
+++ app/src/main/java/com/rafaelkerr/appclientrestspring/app/Greeting.java
@@ -0,0 +1,20 @@
+package com.rafaelkerr.appclientrestspring.app;
+
+/**
+ * Created by rafael on 24/08/14.
+ */
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+
+}
--- app/src/main/java/com/rafaelkerr/appclientrestspring/app/MainActivity.java
+++ app/src/main/java/com/rafaelkerr/appclientrestspring/app/MainActivity.java
@@ -1,9 +1,15 @@
 package com.rafaelkerr.appclientrestspring.app;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 
 public class MainActivity extends Activity {
@@ -19,6 +25,11 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new HttpRequestTask().execute();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,6 +49,33 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Greeting> {
+
+        @Override
+        protected Greeting doInBackground(Void... params) {
+            try {
+                final String url = "http://rest-service.guides.spring.io/greeting";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+                return greeting;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Greeting greeting) {
+            TextView greetingIdText = (TextView) findViewById(R.id.id_value);
+            TextView greetingContentText = (TextView) findViewById(R.id.content_value);
+            greetingIdText.setText(greeting.getId());
+            greetingContentText.setText(greeting.getContent());
+        }
+
     }
 
 }
